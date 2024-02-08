7eff30d6ca70d4c153a8cbc9f6d6db7b5e3809b6
Gene Kogan <kogan.gene@gmail.com>
2024-02-08 11:00:02 -0800

    disable

--- src/pages/api/create.ts
+++ src/pages/api/create.ts
@@ -3,23 +3,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { text_input } = req.body;
-
-  try {
-    const eden = new EdenClient({
-      apiKey: process.env.EDEN_API_KEY,
-      apiSecret: process.env.EDEN_API_SECRET,
-    });
-    const uris = await eden.create({
-      generatorName: "create",
-      config: {
-        text_input,
-      },
-    });
-    return res.status(200).json({ uri: uris[0] });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error" });
-  }
+  return res.status(500).json({ error: "Disabled" });
+  // try {
+  //   const eden = new EdenClient({
+  //     apiKey: process.env.EDEN_API_KEY,
+  //     apiSecret: process.env.EDEN_API_SECRET,
+  //   });
+  //   const uris = await eden.create({
+  //     generatorName: "create",
+  //     config: {
+  //       text_input,
+  //     },
+  //   });
+  //   return res.status(200).json({ uri: uris[0] });
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(500).json({ error: "Internal server error" });
+  // }
 };
 
 export default handler;
