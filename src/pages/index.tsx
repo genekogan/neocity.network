533924f29003106e89cc916ab6829d715825489d
Gene Kogan <kogan.gene@gmail.com>
2024-02-08 11:20:26 -0800

    update image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,55 +4,32 @@ import axios from "axios";
 
 export default function Home() {
   const [prompt, setPrompt] = useState("");
-  const [isCreating, setIsCreating] = useState(false);
-  const [resultImageUrl, setResultImageUrl] = useState(undefined);
+  // const [isCreating, setIsCreating] = useState(false);
+  // const [resultImageUrl, setResultImageUrl] = useState(undefined);
 
-  const handleSubmit = async () => {
-    try {
-      setIsCreating(true);
-      const res = await axios.post("/api/create", { text_input: prompt });
-      setIsCreating(false);
-      setResultImageUrl(res.data.uri);
-    } catch (e) {
-      console.error(e);
-      setIsCreating(false);
-    }
-  };
+  // const handleSubmit = async () => {
+  //   try {
+  //     setIsCreating(true);
+  //     const res = await axios.post("/api/create", { text_input: prompt });
+  //     setIsCreating(false);
+  //     setResultImageUrl(res.data.uri);
+  //   } catch (e) {
+  //     console.error(e);
+  //     setIsCreating(false);
+  //   }
+  // };
   return (
-    <main className="p-4 ">
-      <h1 className="text-4xl font-bold text-center mb-4">Hello Eden</h1>
-      {resultImageUrl && (
-        <div className="flex justify-center">
-          <Image
-            src={resultImageUrl}
-            alt={prompt}
-            width={500}
-            height={500}
-            className="rounded-lg shadow-lg"
-          />
-        </div>
-      )}
-      <div className="mt-4">
-        <label
-          htmlFor="prompt"
-          className="block text-sm font-medium text-white"
-        >
-          Prompt
-        </label>
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
+    <main className="p-4 relative main-content">
+      <h1 className="text-4xl font-bold absolute top-0 left-0 text-white" style={{ padding: '10px' }}>Neocity.network</h1>
+      <div className="flex justify-center">
+        <Image
+          src="https://edenartlab-prod-data.s3.us-east-1.amazonaws.com/13b93f61f282a10f345672d6447a915cb4712b03b2935a81b1e4725e462a20a9.jpg"
+          className="rounded-lg shadow-lg"
+          alt="Neocity.network"
+          width={1600}
+          height={1024}
         />
       </div>
-      <button
-        onClick={handleSubmit}
-        disabled={isCreating}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
-      >
-        {isCreating ? "Creating..." : "Create"}
-      </button>
     </main>
   );
 }
